dc46f121f4928ef073880a13712c8f325893ef96
pamapp <alineus@mail.ru>
2021-12-23 21:20:17 +0300

    Initial commit

---
+++ Tetris/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tetris
+//
+//  Created by Alina Potapova on 23.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by Alina Potapova on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
